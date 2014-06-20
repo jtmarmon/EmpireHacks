2c1ff554a24ea09e39deabb1dec1535991cf2ab5
Jason Marmon <jtmarmon@gmail.com>
2014-06-20 13:32:10 -0400

    added alleyNYC style hover

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-	setTimeout(function(){
+	$("#placeholder").hover(animateIn, animateOut);
+	/*setTimeout(function(){
 		$("#wrapper").animate({
 			'opacity':'1'
 		},700);
-	},1000);
+	},1000);*/
 });
+function animateIn(){
+		$("#wrapper").animate({
+			'opacity':'1',
+			'width':'80%',
+			'height':'100%'
+		},500);
+}
+function animateOut(){
+		$("#wrapper").animate({
+			'opacity':'0',
+			'width':'0',
+			'height':'0'
+		},500);
+}
